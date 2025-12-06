d09bc4dfd02a9b9de7ac527a610e435533688f3d
mohammedrafi26 <rafihajwani1234@gmail.com>
2025-12-06 13:24:25 +0530

    Update movies_app.py

--- movies_app.py
+++ movies_app.py
@@ -4,14 +4,14 @@ from sklearn.neighbors import NearestNeighbors
 import requests
 import streamlit as st
 
-# ================== Streamlit UI ==================
+# =============== App Header ===============
 st.set_page_config(page_title="Movie Recommendation System", layout="wide")
 st.title("🎬 Movie Recommendation System")
 st.write("Get movie recommendations with posters — powered by TMDB!")
 
-# ================== Load Dataset ==================
+# =============== Load Dataset ===============
 try:
-   movies = pd.read_csv("movies.csv", low_memory=False)
+    movies = pd.read_csv("movies.csv", low_memory=False)
     st.success("✅ Dataset loaded successfully!")
 except Exception as e:
     st.error(f"Error loading dataset: {e}")
@@ -96,5 +96,6 @@ if st.button("Recommend"):
 
     
         
+
 
 
